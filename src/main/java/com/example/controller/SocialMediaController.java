f5699833e3fa5e2f9d1e494fa5e7c7c96c12a36d
Remar Supnet <revin122@gmail.com>
2025-04-11 04:46:16 +0000

    User Registration; coded Repository

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,88 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+@RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService accountService;
+
+    /*
+    
+
+## 2: Our API should be able to process User logins.
+
+As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. The request body will contain a JSON representation of an Account.
+
+- The login will be successful if and only if the username and password provided in the request body JSON match a real account existing on the database. If successful, the response body should contain a JSON of the account in the response body, including its accountId. The response status should be 200 OK, which is the default.
+- If the login is not successful, the response status should be 401. (Unauthorized)
+
+
+## 3: Our API should be able to process the creation of new messages.
+
+As a user, I should be able to submit a new post on the endpoint POST localhost:8080/messages. The request body will contain a JSON representation of a message, which should be persisted to the database, but will not contain a messageId.
+
+- The creation of the message will be successful if and only if the messageText is not blank, is not over 255 characters, and postedBy refers to a real, existing user. If successful, the response body should contain a JSON of the message, including its messageId. The response status should be 200, which is the default. The new message should be persisted to the database.
+- If the creation of the message is not successful, the response status should be 400. (Client error)
+
+## 4: Our API should be able to retrieve all messages.
+
+As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages.
+
+- The response body should contain a JSON representation of a list containing all messages retrieved from the database. It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default.
+
+## 5: Our API should be able to retrieve a message by its ID.
+
+As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{messageId}.
+
+- The response body should contain a JSON representation of the message identified by the messageId. It is expected for the response body to simply be empty if there is no such message. The response status should always be 200, which is the default.
+
+## 6: Our API should be able to delete a message identified by a message ID.
+
+As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{messageId}.
+
+- The deletion of an existing message should remove an existing message from the database. If the message existed, the response body should contain the number of rows updated (1). The response status should be 200, which is the default.
+- If the message did not exist, the response status should be 200, but the response body should be empty. This is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond with the same type of response.
+
+## 7: Our API should be able to update a message text identified by a message ID.
+
+As a user, I should be able to submit a PATCH request on the endpoint PATCH localhost:8080/messages/{messageId}. The request body should contain a new messageText values to replace the message identified by messageId. The request body can not be guaranteed to contain any other information.
+
+- The update of a message should be successful if and only if the message id already exists and the new messageText is not blank and is not over 255 characters. If the update is successful, the response body should contain the number of rows updated (1), and the response status should be 200, which is the default. The message existing on the database should have the updated messageText.
+- If the update of the message is not successful for any reason, the response status should be 400. (Client error)
+
+## 8: Our API should be able to retrieve all messages written by a particular user.
+
+As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{accountId}/messages.
+
+- The response body should contain a JSON representation of a list containing all messages posted by a particular user, which is retrieved from the database. It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default.
+
+## 9: The Project utilizes the Spring Framework.
+
+- The project was created leveraging the spring framework, including dependency injection, autowire functionality and/or Spring annotations. 
+     */
+
+    // GET
+
+    // POST
+
+    @PostMapping("register")
+    public ResponseEntity<Account> register(@RequestBody Account account) {
+        if(account != null && account.getUsername() != null && !account.getUsername().isBlank() && account.getPassword().length() >= 4) {
+            Account newAccount = accountService.register(account);
+            if(newAccount != null) 
+                return ResponseEntity.ok().body(newAccount);
+            else
+                return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+    
+
+
+
+    // PATCH
+
+    // DELETE
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,27 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Account register(Account account) {
+        //check if account already exists
+        Optional<Account> accountOptional = accountRepository.findByUsername(account.getUsername());
+        if(accountOptional.isPresent()) {
+            return null;
+        } else {
+            Account accountCreated = accountRepository.save(account);
+            return accountCreated;
+        }   
+    } 
 }
